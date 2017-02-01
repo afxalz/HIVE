d049f4e35c3f0085861265fc0081d26894804af3
afzalhmd14 <afzalhmd14@gmail.com>
2017-02-01 16:48:44 +0530

    Hive-v2.0 debugged ready for combined test

--- Hive-v2.0/Hive-v2.0.ino
+++ Hive-v2.0/Hive-v2.0.ino
@@ -1,19 +1,19 @@
 #include <NewPing.h>
 #include <AccelStepper.h>
 #include <math.h>
-                                                                //#include <Wire.h>
+                                                               				 //#include <Wire.h>
 
-#define SONAR_NUM     6 // Number or sensors.
+#define SONAR_NUM     6 // Number or sensors.						//Macros for ping
 #define MAX_DISTANCE 200 // Maximum distance (in cm) to ping.
 
-unsigned int pingdis[6]={2,2,2,2,2,2},lastping=0;         // Where the ping distances are stored.
+unsigned int pingdis[6]={2,2,2,2,2,2},lastping=0;					// Where the ping distances are stored.
 uint8_t currentSensor = 0;
 double angle=0;   
 
       
 
-NewPing sonar[SONAR_NUM] = {           // Sensor object array.
-  NewPing(10,11, MAX_DISTANCE),//0    // Each sensor's trigger pin, echo pin, and max distance to ping.
+NewPing sonar[SONAR_NUM] = {										// Sensor object array.
+  NewPing(10,11, MAX_DISTANCE),//0  								// Each sensor's trigger pin, echo pin, and max distance to ping.
   NewPing(A2,A3, MAX_DISTANCE),//1           
   NewPing(A4,A5, MAX_DISTANCE),//2           
   NewPing(6,7, MAX_DISTANCE),//3
@@ -24,53 +24,47 @@ NewPing sonar[SONAR_NUM] = {           // Sensor object array.
 #define HALFSTEP 8
 
 // Motor pin definitions
-#define motorPin1  2    // IN1 on the ULN2003 driver 1
-#define motorPin2  3     // IN2 on the ULN2003 driver 1
-#define motorPin3  4     // IN3 on the ULN2003 driver 1
-#define motorPin4  5     // IN4 on the ULN2003 driver 1
+#define motorPin1  2   												 // IN1 on the ULN2003 driver 1
+#define motorPin2  3   												  // IN2 on the ULN2003 driver 1
+#define motorPin3  4    											 // IN3 on the ULN2003 driver 1
+#define motorPin4  5   												  // IN4 on the ULN2003 driver 1
 
-#define motorPin5  A1     // IN1 on the ULN2003 driver 1
-#define motorPin6  A0    // IN2 on the ULN2003 driver 1
-#define motorPin7  13    // IN3 on the ULN2003 driver 1
-#define motorPin8  12   // IN4 on the ULN2003 driver 1
+#define motorPin5  A1    											 // IN1 on the ULN2003 driver 1
+#define motorPin6  A0   											 // IN2 on the ULN2003 driver 1
+#define motorPin7  13    											// IN3 on the ULN2003 driver 1
+#define motorPin8  12   											// IN4 on the ULN2003 driver 1
 
 
-// Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper with 28BYJ-48
+																	// Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper with 28BYJ-48
+
 AccelStepper stepper1(HALFSTEP, motorPin1, motorPin3, motorPin2, motorPin4);
-
-// Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper with 28BYJ-48
 AccelStepper stepper2(HALFSTEP, motorPin5, motorPin7, motorPin6, motorPin8);
 
 
 int checkdis(int last_ping){
 
 	
-	for (uint8_t i = 0; i < SONAR_NUM; i++) {       // Loop through all the sensors.
+	for (uint8_t i = 0; i < SONAR_NUM; i++) {      					 // Loop through all the sensors.
+		
 		Serial.println("sensing");
 		while((millis()-last_ping)<=50);
+		currentSensor = i;                        					  // Sensor being accessed.
+		pingdis[currentSensor]=sonar[currentSensor].ping_cm();
 		
-		//sonar[currentSensor].timer_stop();          // Make sure previous timer is canceled before starting a new ping (insurance).
-		currentSensor = i;                          // Sensor being accessed.
-   pingdis[currentSensor]=sonar[currentSensor].ping_cm();
-   if(pingdis[currentSensor]==0){
-    pingdis[currentSensor]=50;
-   }
-		//sonar[currentSensor].ping_timer(echoCheck);
+		if(pingdis[currentSensor]==0){
+	    	pingdis[currentSensor]=200;
+   		}
+																
                                                   
- /*   Serial.print(i);                            //Data to the processing environment      
-    Serial.print(",");
-    Serial.print(pingdis[currentSensor]);
-    Serial.print(".");*/
-    
-	}
-
-  Serial.println("Distances measured by sensors: ");
  
- for(uint8_t i = 0; i<SONAR_NUM; i++) {
-  Serial.print(i);
-  Serial.print("-----");
-	Serial.println(pingdis[i]);
- }
+
+	Serial.println("Distances measured by sensors: ");
+ 
+	for(uint8_t i = 0; i<SONAR_NUM; i++) {
+		Serial.print(i);
+		Serial.print("-----");
+		Serial.println(pingdis[i]);
+ 	}
 
  
 	return millis();  
@@ -78,106 +72,115 @@ int checkdis(int last_ping){
 
 
 
-/*void echoCheck() {									 // If ping received, set the sensor distance to array.
-Serial.println("echo");
-	//pingdis[currentSensor]=50;		
-	if (sonar[currentSensor].check_timer()){
-		pingdis[currentSensor]= sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
-   Serial.print("measured=");
-   Serial.println(pingdis[currentSensor]);
-	}
-		
-}	*/
-
 double calcDir(){
 
-	double x_dis=0;
+	double x_dis=0;													//Initialising variables
 	double y_dis=0;
 	double angle=0;
 
 	for (uint8_t i = 0; i < SONAR_NUM; i++) { 
-		x_dis=((1.00/pingdis[i])*cos(((i*60)*1000) / 572)) + x_dis;		//Superposition of all distance vectors
- 
-		y_dis=((1.00/pingdis[i])*sin(((i*60)*1000) / 572)) + y_dis;
+	
+		x_dis=((1.00/pingdis[i])*cos(i*1.0472)) + x_dis;			//Superposition of all distance vectors
+    	y_dis=((1.00/pingdis[i])*sin(i*1.0472)) + y_dis;			//1.0472= 60 * rad(1)
   
 	}
 
-	angle=(atan(y_dis/x_dis))* 57296 / 1000;							//Resultant vector dir range = +180 to -180
+	angle=(atan(y_dis/x_dis))* 57296 / 1000;						//Resultant vector dir range = +90 to 90
 
-
-	if(x_dis<0 && y_dis>0){												//handle second quadrant angle
+	if(x_dis<0 && y_dis>0){											//handle second quadrant angle-- range 0 to +180
 
 		angle=angle+180;
 	}
-	if(x_dis<0 && y_dis<0){												//handle fourth quadrant angle
+	if(x_dis<0 && y_dis<0){											//handle fourth quadrant angle-- range 0 to -180
 
 		angle=angle-180;
 	}
 
-  Serial.print("Angle: ");
-  Serial.println(angle);
+	Serial.print("Angle: ");
+	Serial.println(angle);
   
 	return angle;
 }
 
 
-void move(double angle){												//Move to a desired direction
+void move(){											  			//Move in the desired direction
+
+	stepper1.setCurrentPosition(0);									//Set current position to zero
+	stepper2.setCurrentPosition(0);
+
+	stepper1.setMaxSpeed(1000.0);									//Set maxspeed achievable
+    stepper2.setMaxSpeed(1000.0);
+
+    stepper1.setAcceleration(1000.0);								//Set the Acceleration
+    stepper2.setAcceleration(1000.0);
+
+	stepper1.setSpeed(400);											//Set initial speed
+	stepper2.setSpeed(400);
 
 
+	stepper1.moveTo(2048);											//Assign the no. of turns to move
+	stepper2.moveTo(-2048);		
+	
+	Serial.println("Moving forward...");
+	
+	while(stepper1.distanceToGo() != 0&&stepper2.distanceToGo() != 0){					//check if the no. of turns achieved
 
-	if(angle!=0){
+		stepper1.run();
+		stepper2.run(); 
+	}
+	
+	Serial.println("Motion Successful!!");
+		
+}
 
-																		            Serial.println("Turning");
-		stepper1.setCurrentPosition(0);
-		stepper2.setCurrentPosition(0);
 
-		 stepper1.setSpeed(200);
-		 stepper2.setSpeed(200);
-		  
-		stepper1.moveTo(30/**angle*/);										//calculate the real number for an arbitary angle
-		stepper2.moveTo(-30/**angle*/);
-																		              Serial.println("Go-to run");
+void turn(double angle){
 
-		while(stepper1.distanceToGo() != 0&&stepper2.distanceToGo() != 0){
-			stepper1.run();
-			stepper2.run();
-		  
-			}
-																		              Serial.println("Ran");
+	if(!(angle>=(-30)&&angle<=30)){                       			 // check angle range -30 to +30 for forward movement unless turn
+
+	    Serial.println("Initiating turn..");
+	    
+	    stepper1.setCurrentPosition(0);									//Set current position to zero
+	    stepper2.setCurrentPosition(0);
+
+	    stepper1.setMaxSpeed(1000.0);									//Set maxspeed achievable
+	    stepper2.setMaxSpeed(1000.0);
+
+	    stepper1.setAcceleration(1000.0);								//Set acceleration
+	    stepper2.setAcceleration(1000.0);
+	    
+	    stepper1.setSpeed(400);											//Set initial speed
+	    stepper2.setSpeed(400);
+	      
+	    stepper1.moveTo(int(6.82*angle));                			   //calculate the no. of turns from an arbitary angle
+	    stepper2.moveTo(int(6.82*angle));
+	    
+	    Serial.println("Turning...");
+
+	    while(stepper1.distanceToGo() != 0&&stepper2.distanceToGo() != 0){		//check if the no. of turns achieved
+	    	stepper1.run();
+	    	stepper2.run();
+	      
+	    }
+	    
+	    Serial.println("Turned!!");
 	}
 	
 	else{
-
-		stepper1.setCurrentPosition(0);
-		                                                                    //    Serial.println("current pos");
-		                                                                    //   Serial.println(stepper1.currentPosition());
-		                                                                    //   Serial.println(stepper2.currentPosition());
-		stepper2.setCurrentPosition(0);
-
-		stepper1.setSpeed(400);
-		stepper2.setSpeed(400);
-
-
-		stepper1.moveTo(4096);
-		stepper2.moveTo(4096);
-																			              Serial.println("Moving forward");
-		while(stepper1.distanceToGo() != 0&&stepper2.distanceToGo() != 0){
-			stepper1.run();
-			stepper2.run();
-																			               // Serial.println("forward run");
-			}
-		}	
-																	              		  Serial.println("bot-moved");
-		
+    
+    Serial.println("Did not turn!!");									//Report did not turn
+	
+	}
 }
+
 
 
 void setup() {
 
 	Serial.begin(115200);
-																			//Wire.begin();
+																					//Wire.begin();
 	delay(50);
-	lastping=checkdis(millis());
+	lastping=checkdis(millis());										//Initialse search
 
 }
 
@@ -188,13 +191,17 @@ void setup() {
 void loop() {
   
 	lastping=checkdis(lastping);
-	angle=calcDir();
-	move(angle);
+	angle=calcDir();													//Get the right direction to move
+	turn(angle);														//See if a turn is required
+	move();																//Move in the found direction
+
+
 																			/*	Wire.beginTransmission(1); // transmit to device #8
 																				Wire.write(angle);        // sends five bytes
 																				Wire.endTransmission();    // stop transmitting
 																			*/
-	delay(2000);
+																			
+	delay(1000);
 
 }
 
